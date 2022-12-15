f90e66db41c776f31bb801471a0ac28c83f1ccbe
Febi Nurfajar <febinurfajar13@gmail.com>
2022-12-15 22:43:10 +0700

    styling header and footer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/landing/Landing";
-import ProtectRoute from "./routes/ProtectRoute";
-import MainStore from "./stores/index";
-import ErrorPage from "./pages/error/ErrorPage";
-import LoginPage from "./pages/login/LoginPage";
-import "./index.css";
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing/Landing';
+import ProtectRoute from './routes/ProtectRoute';
+import MainStore from './stores/index';
+import ErrorPage from './pages/error/ErrorPage';
+import LoginPage from './pages/login/LoginPage';
+import './index.css';
+import Header from './layouts/Header';
+import TransactionsPage from './pages/transactions/TransactionsPage';
 function App() {
   return (
     <BrowserRouter>
       {/* <Provider store={MainStore}> */}
+
       <Routes>
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path='*' element={<ErrorPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/transactions' element={<TransactionsPage />} />
         <Route element={<ProtectRoute />}>
-          <Route path="" element={<LandingPage />} />
+          <Route path='' element={<LandingPage />} />
         </Route>
       </Routes>
       {/* </Provider> */}
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
+import Footer from '../../layouts/Footer';
+import Header from '../../layouts/Header';
 
 export default function LandingPage() {
   return (
     <>
+      <Header />
       <h1 className='text-3xl font-bold underline'>ini landing milik Febi</h1>
       <Link to='/login'>
         <button>Login</button>
       </Link>
+      <Footer />
     </>
   );
 }
