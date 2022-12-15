990e301bce6c13b7d9df4614679ad9fd881c344b
Febi Nurfajar <febinurfajar13@gmail.com>
2022-12-15 21:17:52 +0700

    add febi to landing page

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">ini landing</h1>
-      <Link to="/login">
+      <h1 className='text-3xl font-bold underline'>ini landing milik Febi</h1>
+      <Link to='/login'>
         <button>Login</button>
       </Link>
     </>
