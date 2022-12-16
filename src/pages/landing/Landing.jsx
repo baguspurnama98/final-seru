5b1d1146ad958dd854e6ae707dca6f19ba6c1c34
Cornelius Bagus Purnama Putra <baguspurnama98@gmail.com>
2022-12-16 08:51:12 +0700

    update landing

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -4,12 +4,12 @@ import logo from "../../assets/apartment-hero.svg";
 export default function LandingPage() {
   return (
     <>
-      <div className="container max-w-5xl mx-auto grid grid-cols-2 py-24">
+      <div className="container max-w-5xl mx-auto grid md:grid-cols-2 sm:grid-cols-1 py-24">
         <div className="my-auto">
-          <h1 className="font-bold font-sans text-5xl pb-5 text-blue-900">
+          <h1 className="font-bold font-sans text-5xl pb-3 text-blue-900">
             Mandiri Apartment
           </h1>
-          <div className="text-3xl font-bold pb-12">Daan Mogot City</div>
+          <div className="text-3xl font-bold pb-10">Daan Mogot City</div>
           <Link
             to="/login"
             className="py-4 px-16 bg-blue-700 hover:bg-blue-500 font-bold text-2xl rounded-md text-white drop-shadow-3xl"
