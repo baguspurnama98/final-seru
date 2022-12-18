0749bd0a22ee7b325ed64e070c89d417d796ba73
Cornelius Bagus Purnama Putra <baguspurnama98@gmail.com>
2022-12-18 23:11:59 +0700

    THE LAST COMMIT

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <div className="border-t-2 border-gray-300 flex flex-col items-center">
           <div className="sm:w-2/3 text-center py-6">
             <p className="text-sm text-black font-bold mb-2">
-              © 2022 by Bagus family
+              © 2022 by Bagus Febi Fitri Maulana Titis
             </p>
           </div>
         </div>
